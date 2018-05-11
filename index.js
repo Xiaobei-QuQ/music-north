56309e674a474c4f29f8135935f47d20e29a5d75
xiaobei <xiaobei.quq@gmail.com>
2018-05-12 00:50:59 +0800

    add newmusci

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 $(function(){
-    $.get('./songs.json').then(function (res) {
+    // $.get('./songs.json').then(function (res) {
+    //     let items = res
+    //     items.forEach((i)=>{
+    //             let $li =$(`
+    //                 <li>
+    //                 <a href="./song.html?id=${i.id}">
+    //                 <h3>${i.title}</h3>
+    //                 <p>${i.author}</p>
+    //                <svg>
+    //                 <use xlink:href="#icon-play1"></use>
+    //                 </svg></a></li>
+    //             </ol>
+    //             `)
+    //             $('#latesMusic').append($li)
+    //         })
+    //         $('.musicLoading').remove()
+    // })
+    $.get('./otherSongs.json').then(function (res) {
         let items = res
         items.forEach((i)=>{
-                let $li =$(`
+            let $li =$(`
                     <li>
-                    <a href="./song.html?id=${i.id}">
-                    <h3>${i.title}</h3>
-                    <p>${i.author}</p>
+                    <a href="./song.html?id=${i.id_}">
+                    <h3>${i.name}</h3>
+                    <p>${i.album} ${i.signer}</p>
                    <svg>
                     <use xlink:href="#icon-play1"></use>
                     </svg></a></li>
                 </ol>
                 `)
-                $('#latesMusic').append($li)
-            })
-            $('.musicLoading').remove()
+            $('#latesMusic').append($li)
+    })
+        $('.musicLoading').remove()
     })
 })
--- song.js
+++ song.js
@@ -1,13 +1,46 @@
 $(function(){
-    let id = location.search.match(/\bid=([^&]*)/)[1]
-    $.get('./songs.json').then(
+     let id = location.search.match(/\bid=([^&]*)/)[1]
+    console.log(id)
+    // $.get('./songs.json').then(
+    //     function (res) {
+    //         let songs = res
+    //         let song = songs.filter(s=>s.id === id)[0]
+    //         let {src} = song
+    //         let audio = document.createElement('audio')
+    //         audio.src = src
+    //             // 'http://link.hhtjim.com/163/551337740.mp3'
+    //         var cover = $('.disc-container .cover')
+    //         if ( cover.css('animation-play-state') === 'running' ) {
+    //             cover.css('animation-play-state','paused')
+    //         }
+    //         audio.setAttribute('autoplay','autoplay')
+    //         audio.setAttribute('volume','0')
+    //         audio.oncanplay = function(){
+    //             audio.play()
+    //             $('.disc-container').addClass('playing')
+    //             cover.css('animation-play-state','running')
+    //         }
+    //         $('.icon-pause').on('click',function () {
+    //             audio.pause()
+    //             $('.disc-container').removeClass('playing')
+    //             cover.css('animation-play-state','paused')
+    //         })
+    //         $('.icon-play').on('click',function () {
+    //             audio.play()
+    //             $('.disc-container').addClass('playing')
+    //             cover.css('animation-play-state','running')
+    //
+    //         })
+    //     }
+    // )
+    $.get('./otherSongs.json').then(
         function (res) {
             let songs = res
-            let song = songs.filter(s=>s.id === id)[0]
-            let {src} = song
+            let song = songs.filter(s=>s.id_ == id)[0]
+            let {id_,name,album,signer} = song
             let audio = document.createElement('audio')
-            audio.src = src
-                // 'http://link.hhtjim.com/163/551337740.mp3'
+            audio.src = 'http://link.hhtjim.com/163/'+id_+'.mp3'
+            // 'http://link.hhtjim.com/163/551337740.mp3'
             var cover = $('.disc-container .cover')
             if ( cover.css('animation-play-state') === 'running' ) {
                 cover.css('animation-play-state','paused')
@@ -28,10 +61,12 @@ $(function(){
                 audio.play()
                 $('.disc-container').addClass('playing')
                 cover.css('animation-play-state','running')
-
             })
         }
     )
+    function initPlayer(url) {
+
+    }
     $.get('./lyric.json').then(function(object){
         let {lyric} = object
         let array = lyric.split('\n')
